c56731c68e876eaaece267a810744532db0f4eab
Priyanshu Verma <vpriyanshu864@gmail.com>
2025-03-20 13:11:53 +0530

    Update my resume

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,7 +1,7 @@
 import handIcon from "../assets/images/hand-icon.png";
 import rightArrow from "../assets/images/right-arrow-white.png";
 import downloadIcon from "../assets/images/download-icon.png";
-import resume from "../assets/My_resume(U).pdf";
+import resume from "../assets/RESUME-PriyanshuVerma.pdf";
 import profilePhoto from "../assets/images/profile_photo.jpg";
 const MainHeader = () => {
   return (
